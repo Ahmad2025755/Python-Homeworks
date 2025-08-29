7cc0c892dbcb2a6921227c370b5468f8d4959a4b
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-29 16:22:23 +0500

    Add files via upload

---
+++ HomeworkSentences1.py
@@ -0,0 +1,9 @@
+sentence1 = "Hi, my name is Ahmad Abdullah!"
+sentence2 = " I love playing football, and playing games."
+sentence3 = " My bestfriends names are, Sabaoon, Abdullah, and Ibaad!"
+sentence4 = " My most favorite subject is Maths"
+sentence5 = " I go to Qatar every winter and summer vacations"
+
+full_sentences = sentence1+sentence2+sentence3+sentence4+sentence5
+
+print(full_sentences)
