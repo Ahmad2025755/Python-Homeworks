cfb295ea5f204ac168f77e9ffab1fe121e0d1b73
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-09-27 14:51:02 +0500

    Add files via upload

---
+++ HomworkCalculator.py
@@ -0,0 +1,10 @@
+def Addition(a, b):
+    result = a + b
+    print("The sum is", result)
+
+def Multiplication(a, b):
+    result = a * b
+    print("The product is", result)
+
+Addition(660, 350) #Enter the numbers here you want to add
+Multiplication(900, 3) #Enter the numbers here you want to multiply
